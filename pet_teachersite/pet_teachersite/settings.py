d3c0a4b801d4df52ab68be806b85c8b972d46059
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-02-24 22:52:51 +0300

    Изменены настройки

--- pet_teachersite/pet_teachersite/settings.py
+++ pet_teachersite/pet_teachersite/settings.py
@@ -82,6 +82,10 @@ WSGI_APPLICATION = 'pet_teachersite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
